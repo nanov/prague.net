93d94d15b31476e84661b9981c4e6dba073f9a85
nanov <mitko.n@gmail.com>
2026-07-21 13:32:00 +0300

    perf: scaffold Prague.Baseline.Bdn

---
+++ perf/Prague.Baseline.Bdn/Program.cs
@@ -0,0 +1,8 @@
+namespace Prague.Baseline.Bdn;
+
+using BenchmarkDotNet.Running;
+
+internal static class Program {
+	private static void Main(string[] args) =>
+		BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
